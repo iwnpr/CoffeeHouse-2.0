1715343d728c0d238a82de2c4958c29a8954eb8f
Ivan Piryutko <swmngpoor@gmail.com>
2025-08-25 18:18:19 +0300

    Замена типов Id

--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -2,7 +2,7 @@ namespace OrderService.Models;
 
 public class Order
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public List<OrderItem> OrderItems { get; set; } = new();
     public string Status { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
--- OrderService/Models/OrderItem.cs
+++ OrderService/Models/OrderItem.cs
@@ -2,7 +2,7 @@ namespace OrderService.Models;
 
 public class OrderItem
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public int OrderId { get; set; }
     public int MenuItemId { get; set; }
     public int Quantity { get; set; }
